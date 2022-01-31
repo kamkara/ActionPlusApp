44c46b055e605b17ecf971d811181933486719c4
kamkara <lnclass.kamkara@gmail.com>
2022-01-31 16:35:14 +0000

    app/helpers/application_helper: add onglet message and description of site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ def title
   "#{@title} | #{base_title}"
   end
 end
+
+#site description
+def description
+  "Action+ ......"
+end
+
+  #Sign in and up anywhere on app
+    def resource_name
+    :user
+  end
+  
+  def resource
+    @resource ||= User.new
+  end
+  
+  def resource_class
+    User
+  end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
