c13e9f964a3de95ba4cc3213ee70bfd2418fcb0e
kamkara <lnclass.kamkara@gmail.com>
2022-01-31 16:46:48 +0000

    app/controllers: add factorise user authenticate controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :current_user_auhenticate
+    before_action :current_user_unauhenticate
+    
+    
+    private 
+    #redirect current_user to feed if signed
+    def current_user_auhenticate?
+      redirect_to feeds_path if user_signed_in?
+    end
+
+    #redirect current_user to feed if  not signed
+    def current_user_unauhenticate
+      redirect_to root_path  if !user_signed_in?
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
+  
+  before_action :set_user_signed_in?, only: %i[index]
+
+
   def index
   end
-
+  
   def add
+  end
+  
+  private
+  def set_user_signed_in?    
+    redirect_to feeds_path if user_signed_in?
   end
 end
