29ab42c32f443149724e82d41e79b54846d4a672
kamkara <lnclass.kamkara@gmail.com>
2022-01-31 16:32:57 +0000

    add onglet message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    #Onglet Title
+def title
+ base_title = "Action+ Association"
+  if @title.nil?
+  base_title
+  else
+  "#{@title} | #{base_title}"
+  end
+end
 end
