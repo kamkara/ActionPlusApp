6012d5570678fc74690ad31b53f2d1b467f27178
kamkara <lnclass.kamkara@gmail.com>
2022-01-31 15:13:19 +0000

    add devise custom params

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get "added", to:'welcome#add'
-  
-  devise_for :users
+
+  devise_for  :users,
+              :path => '',
+              :path_names => {  :sign_in =>       'connexion',
+                                :sign_out =>      'logout',
+                                :sign_up =>       '',
+                                :registration =>  'inscription',
+                                :edit =>          'edit'
+                              }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
