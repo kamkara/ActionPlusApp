93c3539f34bfc430a925c580f9ac2f9f526f95b6
kamkara <lnclass.kamkara@gmail.com>
2022-02-05 21:53:54 +0000

    add home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,9 +28,7 @@ class ApplicationController < ActionController::Base
       def after_sign_up_path_for(resource)
         dashboard_path
       end
-      def after_sign_out_path_for(scope)
-        root_path
-      end
+      
        
     
   end
