39869a6e08bbfda8b15532d54891f75b1238f0c2
kamkara <lnclass.kamkara@gmail.com>
2022-02-05 20:08:58 +0000

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,12 @@ Rails.application.routes.draw do
   devise_for  :users,
               :path => '',
               :path_names =>
-              { :sign_in =>       'connexion',
-                :sign_out =>      'logout',
-                :sign_up =>         "",
-                :registration =>  'inscription',
-                :edit =>          'edit'
-              }
+                  {  :sign_in =>       'connexion',
+                                :sign_out =>      'logout',
+                                :sign_up =>       '',
+                                :registration =>  'inscription',
+                                :edit =>          'edit'
+                              }
                               
               
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
