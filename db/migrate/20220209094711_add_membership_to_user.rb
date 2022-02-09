6cc8e9f52036502314a9573ae293200735016242
kamkara <lnclass.kamkara@gmail.com>
2022-02-09 10:18:55 +0000

    add mission page

---
+++ db/migrate/20220209094711_add_membership_to_user.rb
@@ -0,0 +1,5 @@
+class AddMembershipToUser < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_160906) do
+ActiveRecord::Schema.define(version: 2022_02_09_094711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
